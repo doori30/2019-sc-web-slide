459168d77c38f2565824399019c9b7391dbe0419
doori30 <rublju0070@naver.com>
2019-10-18 15:30:39 +0900

    slide1-스스로 이동 slide2-pager를 이동하여 이동

--- js/slide2.js
+++ js/slide2.js
@@ -1,14 +1,25 @@
-(function(){
-	var now = 1;
-	var interval = setInterval(slide, 3000);
-	function slide(){
-		$(".banners").stop().animate({"left":(-720*now)+"px"},500, function(){
-			//now++;
-			if(now == 5){
-				now = 1;
-				$(this).css({"left":0});
+var now = 0;
+$(".bt-prev").click(function () {
+	if (now > 0) now--;
+	ani();
+}).hide();
+$(".bt-next").click(function () {
+	if (now < 4) now++;
+	ani();
+});
+
+function ani() {
+	$(".banners").stop().animate({
+				"left": (-720 * now) + "px"
+			}, 500);
+				if (now == 0) {
+					$(".bt-prev").hide();
+					$(".bt-next").show();
+				} else if (now == 4) {
+					$(".bt-prev").show();
+					$(".bt-next").hide();
+				} else {
+					$(".bt-prev").show();
+					$(".bt-next").show();
+				}
 			}
-			else now++;
-		});
-	};
-})();
