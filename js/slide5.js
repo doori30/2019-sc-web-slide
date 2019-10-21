cbe552593ef5b5d9e52ced0f0d715fe99d1af46a
doori30 <rublju0070@naver.com>
2019-10-21 12:00:44 +0900

    slide5가로버튼(자동)

--- js/slide5.js
+++ js/slide5.js
@@ -1,5 +1,23 @@
+/* 
+변수타입
+primitive type :Number(0==false), String(""==false), Boolean, undefined==false, null=false
+->0을 제외한 숫자는 존재하고 빈값이나 0은 존재하지 않는다.
+refernce type : Array, Object 
+							 //주소참조
+const a = [];// 빈 배열과 빈 객체도 주소부가 존재하기 때문에 true
+a[1] = 10; //ok
+a=[1,2,3]; //Error
+
+const b = 5;
+b = 10; //Error
+
+*/
+
 //전역변수 
 var now = 0;
+var interval;
+
+
 
 //EVENT
 $(".bt-prev").click(function () {
@@ -14,11 +32,18 @@ $(".pager").click(function(){
 	now = $(this).index();
 	init()
 });
+//interval CallBack
+function intervalCb(){
+	now++;
+	init();
+	}
 
 //동작(기능)
 //시작할 때 실행됨.
 (function (){
 	pagerInit();
+	interval = setInterval(intervalCb, 2000);
+//interval실행					실행될 함수 ,gap(시간)
 })();
 //이벤트 발생할 때 실행되는 함수
 function init(){
@@ -28,12 +53,19 @@ function init(){
 }
 //애니메이션
 function ani() {
-	$(".banners").stop().animate({
-				"left": (-720 * now) + "px"
-			}, 500);
+	$(".banners").stop().animate({"left": (-720 * now) + "px"
+}, 500,function(){
+			if(now == 5){
+				now = 0;
+				$(".banners").css({"left":0});
+			pagerInit();
+			btInit();
+			}
+			});
 		}
 //버튼정렬
 function btInit(){
+	pagerInit();
 				if (now == 0) {
 					$(".bt-prev").hide();
 					$(".bt-next").show();
